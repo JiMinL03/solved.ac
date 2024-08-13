2e43d448bf9b54c5e2f69bc09126e0e70250cac0
이지민 <147581729+JiMinL03@users.noreply.github.com>
2024-08-13 11:15:00 +0900

    Create 1964.cpp

---
+++ CodingTest/5week/1964.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	long long ans = 5;
+	long long num = 7;
+	for (int i = 2; i <= n; i++)
+	{
+		ans += num;
+		num += 3;
+		ans = ans % 45678;
+	}
+	cout << ans << endl;
+	return 0;
+}
